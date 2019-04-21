828a5223a0bb625d15ee74ad937c605b1ded5c0d
ttxhkx <43335832+ttxhkx@users.noreply.github.com>
2019-04-21 14:18:26 +0800

    4.21.1

--- app/src/main/java/com/example/baidupostbar/LoginActivity.java
+++ app/src/main/java/com/example/baidupostbar/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.example.baidupostbar;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText et_account;
@@ -26,6 +32,48 @@ public class LoginActivity extends AppCompatActivity {
         btn_forgetPassword = findViewById(R.id.btn_forgetPassword);
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
+
+        et_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                 if(!et_password.getText().toString().equals("")&&!et_account.getText().toString().equals("")){
+                     btn_login.setEnabled(true);
+                     btn_login.setBackgroundColor(Color.parseColor("#6495ED"));
+                 }else {
+                     btn_login.setEnabled(false);
+                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                         btn_login.setBackground(getResources().getDrawable(R.drawable.btn_rounded));
+                     }
+
+                 }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = et_account.getText().toString();
+                String password = et_password.getText().toString();
+                //post
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this,RegisterEmail.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/baidupostbar/MainActivity.java
+++ app/src/main/java/com/example/baidupostbar/MainActivity.java
@@ -7,20 +7,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn_1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,RegisterEmail.class);
-                startActivity(intent);
 
-            }
-        });
     }
 }
