dd5ef08fb4dc6a341a18f275826aa9a5aff0e37f
ttxhkx <43335832+ttxhkx@users.noreply.github.com>
2019-04-21 14:20:10 +0800

    4.21.1

--- app/src/main/java/com/example/baidupostbar/MainActivity.java
+++ app/src/main/java/com/example/baidupostbar/MainActivity.java
@@ -7,11 +7,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
     }
 }
