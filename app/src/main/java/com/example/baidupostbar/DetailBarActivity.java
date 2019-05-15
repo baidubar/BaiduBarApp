34c0282ec0c99713435b220d7289ee36c9be4fb2
1727642955@qq.com <43127398+iwannaeat@users.noreply.github.com>
2019-05-15 13:18:45 +0800

    首页推荐基本完成

--- app/src/main/java/com/example/baidupostbar/DetailBarActivity.java
+++ app/src/main/java/com/example/baidupostbar/DetailBarActivity.java
@@ -1,24 +1,49 @@
 package com.example.baidupostbar;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.example.baidupostbar.Adapter.PostAdapter;
 import com.example.baidupostbar.bean.Post;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class DetailBarActivity extends AppCompatActivity {
+import cn.bingoogolapple.baseadapter.BGAOnRVItemClickListener;
+import cn.bingoogolapple.baseadapter.BGAOnRVItemLongClickListener;
+import cn.bingoogolapple.baseadapter.BGARecyclerViewAdapter;
+import cn.bingoogolapple.baseadapter.BGAViewHolderHelper;
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPreviewActivity;
+import cn.bingoogolapple.photopicker.imageloader.BGARVOnScrollListener;
+import cn.bingoogolapple.photopicker.widget.BGANinePhotoLayout;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class DetailBarActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks, BGANinePhotoLayout.Delegate, BGAOnRVItemClickListener, BGAOnRVItemLongClickListener {
     private ArrayList<Post> mDataList;
     private RecyclerView mRecyclerView;
     private FloatingActionButton fab;
+
+    private static final int PRC_PHOTO_PREVIEW = 1;
+
+    private static final int RC_ADD_MOMENT = 1;
+
+    private RecyclerView mMomentRv;
+    private PostAdapter postAdapter;
+
+    private BGANinePhotoLayout mCurrentClickNpl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,39 +56,219 @@ public class DetailBarActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(DetailBarActivity.this,CreatePostActivity.class);
                 startActivity(intent);
+                startActivityForResult(new Intent(DetailBarActivity.this, CreatePostActivity.class),1);
             }
         });
-        initView();
-        initData();
-        initAdapter();
-    }
-    private void initView() {
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
+        mMomentRv = findViewById(R.id.recyclerView);
+        postAdapter = new PostAdapter(mMomentRv);
+        postAdapter.setOnRVItemClickListener(this);
+        postAdapter.setOnRVItemLongClickListener(this);
 
-    @SuppressWarnings("unchecked")
-    private void initAdapter() {
-        BaseQuickAdapter postAdapter = new PostAdapter(R.layout.item_post, mDataList);
-        postAdapter.openLoadAnimation();
-        View top = getLayoutInflater().inflate(R.layout.header_detail_bar, (ViewGroup) mRecyclerView.getParent(), false);
-        postAdapter.addHeaderView(top);
-        postAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                Intent intent = new Intent(DetailBarActivity.this,DetailPost.class);
-                startActivity(intent);
+        mMomentRv.addOnScrollListener(new BGARVOnScrollListener(DetailBarActivity.this));
+        mMomentRv.setLayoutManager(new LinearLayoutManager(this));
+        mMomentRv.setAdapter(postAdapter);
+
+        addNetImageTestData();
+//        initData();
+//        initAdapter();
+    }
+    private void addNetImageTestData() {
+        List<Post> moments = new ArrayList<>();
+
+        moments.add(new Post("1张网络图片", new ArrayList<>(Arrays.asList("http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg"))));
+        moments.add(new Post("2张网络图片", new ArrayList<>(Arrays.asList("http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg", "http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg"))));
+        moments.add(new Post("9张网络图片", new ArrayList<>(Arrays.asList("http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg", "http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg", "http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg", "http://d.hiphotos.baidu.com/image/h%3D200/sign=201258cbcd80653864eaa313a7dca115/ca1349540923dd54e54f7aedd609b3de9c824873.jpg", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered16.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered17.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered18.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered19.png"))));
+        moments.add(new Post("5张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png"))));
+        moments.add(new Post("3张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered4.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered5.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered6.png"))));
+        moments.add(new Post("8张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered16.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered17.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered18.png"))));
+        moments.add(new Post("4张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered7.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered8.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered9.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered10.png"))));
+        moments.add(new Post("2张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered2.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered3.png"))));
+        moments.add(new Post("3张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered4.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered5.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered6.png"))));
+        moments.add(new Post("4张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered7.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered8.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered9.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered10.png"))));
+        moments.add(new Post("9张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered16.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered17.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered18.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered19.png"))));
+        moments.add(new Post("1张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered1.png"))));
+        moments.add(new Post("5张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png"))));
+        moments.add(new Post("6张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered16.png"))));
+        moments.add(new Post("7张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered16.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered17.png"))));
+        moments.add(new Post("8张网络图片", new ArrayList<>(Arrays.asList("http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered11.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered12.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered13.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered14.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered15.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered16.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered17.png", "http://7xk9dj.com1.z0.glb.clouddn.com/refreshlayout/images/staggered18.png"))));
+
+        postAdapter.setData(moments);
+    }
+    /**
+     * 图片预览，兼容6.0动态权限
+     */
+    @AfterPermissionGranted(PRC_PHOTO_PREVIEW)
+    private void photoPreviewWrapper() {
+        if (mCurrentClickNpl == null) {
+            return;
+        }
+
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            File downloadDir = new File(Environment.getExternalStorageDirectory(), "BGAPhotoPickerDownload");
+            BGAPhotoPreviewActivity.IntentBuilder photoPreviewIntentBuilder = new BGAPhotoPreviewActivity.IntentBuilder(this)
+                    .saveImgDir(downloadDir); // 保存图片的目录，如果传 null，则没有保存图片功能
+
+            if (mCurrentClickNpl.getItemCount() == 1) {
+                // 预览单张图片
+                photoPreviewIntentBuilder.previewPhoto(mCurrentClickNpl.getCurrentClickItem());
+            } else if (mCurrentClickNpl.getItemCount() > 1) {
+                // 预览多张图片
+                photoPreviewIntentBuilder.previewPhotos(mCurrentClickNpl.getData())
+                        .currentPosition(mCurrentClickNpl.getCurrentClickItemPosition()); // 当前预览图片的索引
             }
-        });
-
-        mRecyclerView.setAdapter(postAdapter);
-    }
-
-    private void initData() {
-        mDataList = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            Post item = new Post();
-            mDataList.add(item);
+            startActivity(photoPreviewIntentBuilder.build());
+        } else {
+            EasyPermissions.requestPermissions(this, "图片预览需要以下权限:\n\n1.访问设备上的照片", PRC_PHOTO_PREVIEW, perms);
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (requestCode == PRC_PHOTO_PREVIEW) {
+            Toast.makeText(this, "您拒绝了「图片预览」所需要的相关权限!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClickNinePhotoItem(BGANinePhotoLayout ninePhotoLayout, View view, int position, String model, List<String> models) {
+        mCurrentClickNpl = ninePhotoLayout;
+        photoPreviewWrapper();
+    }
+
+    //帖子的点击事件
+    @Override
+    public void onRVItemClick(ViewGroup viewGroup, View view, int position) {
+        Toast.makeText(this, "点击了item " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    //帖子的长按事件
+    @Override
+    public boolean onRVItemLongClick(ViewGroup viewGroup, View view, int position) {
+        //Toast.makeText(getContext(), "长按了item " + position, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    private class PostAdapter extends BGARecyclerViewAdapter<Post> {
+
+
+        //重写viewHolder方法，给recyclerView添加头部
+//        @NonNull
+//        @Override
+//        public BGARecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            View view = null;
+//            if (viewType == 0) {  //根据不同的viewtype,加载不同的布局
+//                view = LayoutInflater.from(DetailBarActivity.this).inflate(R.layout.header_detail_bar, parent,false);
+//                return new MyViewHolderOne(view);
+//            } else {
+//                view = LayoutInflater.from(DetailBarActivity.this).inflate(R.layout.item_post,parent, false);
+//                return new MyViewHolderTwo(view);
+//            }
+//        }
+        //用position判断当前位置是否是头部
+        @Override
+        public int getItemViewType(int position) {
+            if (position == 0) {  //根据你的条件，返回不同的type
+                return 0;
+            } else {
+                return 1;
+            }
+
+        }
+        //重写onBindViewHolder方法，绑定两个不同的holder
+//        @Override
+//        public void onBindViewHolder(BGARecyclerViewHolder viewHolder, final int position) {
+//
+//            switch (getItemViewType(position)) {
+//                case 0:  //不同的布局，做不同的事
+//                    MyViewHolderOne holderOne = (MyViewHolderOne) holder;
+//
+//                    break;
+//                case 1:
+//                    final MyViewHolderTwo holderTwo = (MyViewHolderTwo) holder;
+//            }
+//
+//            public int getItemCount () {
+//                return list.size();
+//            }
+//        }
+//
+//
+//            class MyViewHolderOne extends BGARecyclerViewHolder {
+//
+//                TextView item_word_date;  //日期
+//
+//                public MyViewHolderOne(View view) {
+//                    super(view);
+//                    item_word_date = (TextView) view.findViewById(R.id.item_word_date);
+//                }
+//            }
+//
+//
+//            class MyViewHolderTwo extends BGARecyclerViewHolder {
+//
+//                TextView item_word;    //单词
+//                TextView item_word_mean;    //词义
+//
+//                public MyViewHolderTwo(View view) {
+//                    super(view);
+//                    item_word = (TextView) view.findViewById(R.id.item_word);
+//                    item_word_mean = (TextView) view.findViewById(R.id.item_word_mean);
+//                }
+//            }
+
+
+
+        public PostAdapter(RecyclerView recyclerView) {
+            super(recyclerView, R.layout.item_post);
+        }
+
+        @Override
+        protected void fillData(BGAViewHolderHelper helper, int position, Post moment) {
+            if (TextUtils.isEmpty(moment.content)) {
+                helper.setVisibility(R.id.tv_content, View.GONE);
+            } else {
+                helper.setVisibility(R.id.tv_content, View.VISIBLE);
+                helper.setText(R.id.tv_content, moment.content);
+            }
+            BGANinePhotoLayout ninePhotoLayout = helper.getView(R.id.npl_item_moment_photos);
+            ninePhotoLayout.setDelegate(DetailBarActivity.this);
+            ninePhotoLayout.setData(moment.photos);
+        }
+    }
+
+//    @SuppressWarnings("unchecked")
+//    private void initAdapter() {
+//        BaseQuickAdapter postAdapter = new PostAdapter(R.layout.item_post, mDataList);
+//        postAdapter.openLoadAnimation();
+//        View top = getLayoutInflater().inflate(R.layout.header_detail_bar, (ViewGroup) mRecyclerView.getParent(), false);
+//        postAdapter.addHeaderView(top);
+//        postAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+//                Intent intent = new Intent(DetailBarActivity.this,DetailPost.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        mRecyclerView.setAdapter(postAdapter);
+//    }
+//
+//    private void initData() {
+//        mDataList = new ArrayList<>();
+//        for (int i = 0; i < 8; i++) {
+//            Post item = new Post();
+//            mDataList.add(item);
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/baidupostbar/bean/BarHeader.java
@@ -0,0 +1,5 @@
+package com.example.baidupostbar.bean;
+
+//这个是吧的详情页头部的吧信息部分（position == 0）
+public class BarHeader {
+}
