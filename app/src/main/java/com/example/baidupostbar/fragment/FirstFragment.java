ca88b74736292c37759ee788b1a8639d1007e2fd
1727642955@qq.com <43127398+iwannaeat@users.noreply.github.com>
2019-05-21 21:00:21 +0800

    Merge branch 'master' of https://github.com/baidubar/BaiduBarApp into iwannaeat0
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/baidupostbar/fragment/FirstFragment.java
+++ app/src/main/java/com/example/baidupostbar/fragment/FirstFragment.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.baidupostbar.DetailPost;
 import com.example.baidupostbar.R;
 import com.example.baidupostbar.Utils.CheckNetUtil;
